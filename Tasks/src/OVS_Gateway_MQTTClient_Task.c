70039c3e0dc1e0eac21bb0c2a4df3efe01102c1e
Marcel Mello <marcel.mello@qogni.com>
2017-02-13 19:05:00 -0200

    esqueleto de MQTT Task

--- Tasks/src/OVS_Gateway_MQTTClient_Task.c
+++ Tasks/src/OVS_Gateway_MQTTClient_Task.c
@@ -36,6 +36,76 @@ Network network =
 
 uint8_t rxBuf[MQTT_BUFFER_SIZE],txBuf[MQTT_BUFFER_SIZE];
 
+void NetworkInit(Network* n)
+{
+	n->my_socket = 0;
+	n->mqttread = FreeRTOS_read;
+	n->mqttwrite = FreeRTOS_write;
+	n->disconnect = FreeRTOS_disconnect;
+}
+
+void TimerInit(Timer* timer)
+{
+	timer->xTicksToWait = 0;
+	memset(&timer->xTimeOut, '\0', sizeof(timer->xTimeOut));
+}
+
+void MutexInit(Mutex* mutex)
+{
+	mutex->sem = xSemaphoreCreateMutex();
+}
+
+int FreeRTOS_read(Network* n, unsigned char* buffer, int len, int timeout_ms)
+{
+	TickType_t xTicksToWait = timeout_ms / portTICK_PERIOD_MS; /* convert milliseconds to ticks */
+	TimeOut_t xTimeOut;
+	int recvLen = 0;
+
+	vTaskSetTimeOutState(&xTimeOut); /* Record the time at which this function was entered. */
+	do
+	{
+		if (xQueueReceive(*n->SockRxQueue, buffer, xTicksToWait/10))
+		{
+			recvLen++;
+			buffer++;
+		}
+	} while (recvLen < len
+			&& xTaskCheckForTimeOut(&xTimeOut, &xTicksToWait) == pdFALSE);
+
+	return recvLen;
+}
+
+int FreeRTOS_write(Network* n, unsigned char* buffer, int len, int timeout_ms)
+{
+	TickType_t xTicksToWait = timeout_ms / portTICK_PERIOD_MS; /* convert milliseconds to ticks */
+	TimeOut_t xTimeOut;
+	int sentLen = 0;
+
+	vTaskSetTimeOutState(&xTimeOut); /* Record the time at which this function was entered. */
+	do
+	{
+		int rc = 0;
+
+//		FreeRTOS_setsockopt(n->my_socket, 0, FREERTOS_SO_RCVTIMEO, &xTicksToWait, sizeof(xTicksToWait));
+//		rc = FreeRTOS_send(n->my_socket, buffer + sentLen, len - sentLen, 0);
+		rc = xQueueSend(*n->SockTxQueue, buffer++, xTicksToWait/10);
+		if (rc > 0)
+			sentLen++;
+		else if (rc == 0)
+		{
+			break;
+		}
+	} while (sentLen < len
+			&& xTaskCheckForTimeOut(&xTimeOut, &xTicksToWait) == pdFALSE);
+
+	return sentLen;
+}
+
+void FreeRTOS_disconnect(Network* n)
+{
+//	FreeRTOS_closesocket(n->my_socket);
+}
+
 static qog_Task MQTTClientTaskImpl(Gateway * gwInst)
 {
 	network.SockRxQueue = &gwInst->SocketRxQueue;
