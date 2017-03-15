c017e20ba33179bfeddeedab43ad905753f5e05d
Marcel Mello <marcel.mello@qogni.com>
2017-03-15 19:28:44 -0300

    MQTT CLient basico funcionando e portado para Rx/Tx com filas RTOS

--- Tasks/src/OVS_Gateway_MQTTClient_Task.c
+++ Tasks/src/OVS_Gateway_MQTTClient_Task.c
@@ -16,17 +16,12 @@
 //-------- Gateway Interface - START
 #include "qog_ovs_gateway_internal_types.h"
 
-#define WIFI_TASK_HEAP 0x200
-
-static qog_Task MQTTClientTaskImpl(Gateway * gwInst);
-
-qog_gateway_task MQTTClientTaskDef =
-{ &MQTTClientTaskImpl, WIFI_TASK_HEAP, NULL };
+#define MQTT_PUBLISHER_TASK_HEAP 0x200
 
 static qog_Task MQTTPublisherTaskImpl(Gateway * gwInst);
 
 qog_gateway_task MQTTPublisherTaskDef =
-{ &MQTTPublisherTaskImpl, WIFI_TASK_HEAP, NULL };
+{ &MQTTPublisherTaskImpl, MQTT_PUBLISHER_TASK_HEAP, NULL };
 //-------- Gateway Interface - END
 
 MQTTClient client =
@@ -36,153 +31,106 @@ Network network =
 
 uint8_t rxBuf[MQTT_BUFFER_SIZE], txBuf[MQTT_BUFFER_SIZE];
 
-void NetworkInit(Network* n)
+static enum
 {
-	n->my_socket = 0;
-	n->mqttread = FreeRTOS_read;
-	n->mqttwrite = FreeRTOS_write;
-	n->disconnect = FreeRTOS_disconnect;
-}
+	MQTT_CLIENT_UNNINITIALIZED = 0,
+	MQTT_CLIENT_CONNECTED,
+	MQTT_CLIENT_DISCONNECTED
+} MQTTClientState;
 
-void TimerInit(Timer* timer)
+messageHandler MQTTHandler_Info(MessageData * data)
 {
-	timer->xTicksToWait = 0;
-	memset(&timer->xTimeOut, '\0', sizeof(timer->xTimeOut));
-}
-
-int TimerLeftMS(Timer* timer)
-{
-	xTaskCheckForTimeOut(&timer->xTimeOut, &timer->xTicksToWait); /* updates xTicksToWait to the number left */
-	return (timer->xTicksToWait < 0) ?
-			0 : (timer->xTicksToWait * portTICK_PERIOD_MS);
-}
-
-char TimerIsExpired(Timer* timer)
-{
-	return xTaskCheckForTimeOut(&timer->xTimeOut, &timer->xTicksToWait)
-			== pdTRUE;
-}
-
-void TimerCountdown(Timer* timer, unsigned int timeout)
-{
-	TimerCountdownMS(timer, timeout * 1000);
-}
-
-void TimerCountdownMS(Timer* timer, unsigned int timeout_ms)
-{
-	timer->xTicksToWait = timeout_ms / portTICK_PERIOD_MS; /* convert milliseconds to ticks */
-	vTaskSetTimeOutState(&timer->xTimeOut); /* Record the time at which this function was entered. */
-}
-
-int MutexLock(Mutex* mutex)
-{
-	return xSemaphoreTake(mutex->sem, portMAX_DELAY);
-}
-
-int MutexUnlock(Mutex* mutex)
-{
-	return xSemaphoreGive(mutex->sem);
-}
-
-int ThreadStart(Thread* thread, void (*fn)(void*), void* arg)
-{
-	int rc = 0;
-	uint16_t usTaskStackSize = (configMINIMAL_STACK_SIZE * 5);
-	UBaseType_t uxTaskPriority = uxTaskPriorityGet(NULL); /* set the priority as the same as the calling task*/
-
-	rc = xTaskCreate(fn, /* The function that implements the task. */
-	"MQTTTask", /* Just a text name for the task to aid debugging. */
-	usTaskStackSize, /* The stack size is defined in FreeRTOSIPConfig.h. */
-	arg, /* The task parameter, not used in this case. */
-	uxTaskPriority, /* The priority assigned to the task is defined in FreeRTOSConfig.h. */
-	&thread->task); /* The task handle is not used. */
-
-	return rc;
-}
-
-void MutexInit(Mutex* mutex)
-{
-	mutex->sem = xSemaphoreCreateMutex();
-}
-
-int FreeRTOS_read(Network* n, unsigned char* buffer, int len, int timeout_ms)
-{
-	TickType_t xTicksToWait = timeout_ms / portTICK_PERIOD_MS; /* convert milliseconds to ticks */
-	TimeOut_t xTimeOut;
-	int recvLen = 0;
-
-	vTaskSetTimeOutState(&xTimeOut); /* Record the time at which this function was entered. */
-	do
-	{
-		if (xQueueReceive(*n->SockRxQueue, buffer, xTicksToWait / 10))
-		{
-			recvLen++;
-			buffer++;
-		}
-	} while (recvLen < len
-			&& xTaskCheckForTimeOut(&xTimeOut, &xTicksToWait) == pdFALSE);
-
-	return recvLen;
-}
-
-int FreeRTOS_write(Network* n, unsigned char* buffer, int len, int timeout_ms)
-{
-	TickType_t xTicksToWait = timeout_ms / portTICK_PERIOD_MS; /* convert milliseconds to ticks */
-	TimeOut_t xTimeOut;
-	int sentLen = 0;
-
-	vTaskSetTimeOutState(&xTimeOut); /* Record the time at which this function was entered. */
-	do
-	{
-		int rc = 0;
-
-//		FreeRTOS_setsockopt(n->my_socket, 0, FREERTOS_SO_RCVTIMEO, &xTicksToWait, sizeof(xTicksToWait));
-//		rc = FreeRTOS_send(n->my_socket, buffer + sentLen, len - sentLen, 0);
-		rc = xQueueSend(*n->SockTxQueue, buffer++, xTicksToWait / 10);
-		if (rc > 0)
-			sentLen++;
-		else if (rc == 0)
-		{
-			break;
-		}
-	} while (sentLen < len
-			&& xTaskCheckForTimeOut(&xTimeOut, &xTicksToWait) == pdFALSE);
-
-	return sentLen;
-}
-
-void FreeRTOS_disconnect(Network* n)
-{
-//	FreeRTOS_closesocket(n->my_socket);
-}
-
-static qog_Task MQTTClientTaskImpl(Gateway * gwInst)
-{
-	Gateway* gw = (Gateway*) gwInst;
-	network.SockRxQueue = &gw->SocketRxQueue;
-	network.SockTxQueue = &gw->SocketTxQueue;
-	NetworkInit(&network);
-	MQTTClientInit(&client, &network, MQTT_TIMEOUT_MS, txBuf, MQTT_BUFFER_SIZE,
-			rxBuf, MQTT_BUFFER_SIZE);
-
-	for (;;)
-	{
-		osDelay(1000);
-	}
+	uint32_t asd = 123;
+	asd++;
 }
 
 static qog_Task MQTTPublisherTaskImpl(Gateway * gwInst)
 {
+	pb_ostream_t ostream;
+	MQTTMessage msg;
+	uint8_t topic[32];
+
 	Gateway* gw = (Gateway*) gwInst;
-	network.SockRxQueue = &gw->SocketRxQueue;
-	network.SockTxQueue = &gw->SocketTxQueue;
+	Timer timer;
+	network.SockRxQueue = gw->SocketRxQueue;
+	network.SockTxQueue = gw->SocketTxQueue;
 	NetworkInit(&network);
 	MQTTClientInit(&client, &network, MQTT_TIMEOUT_MS, txBuf, MQTT_BUFFER_SIZE,
 			rxBuf, MQTT_BUFFER_SIZE);
 
+	MQTTPacket_connectData conn = MQTTPacket_connectData_initializer;
+	conn.username.cstring = gw->BrokerParams.Username;
+	conn.password.cstring = gw->BrokerParams.Password;
+	conn.clientID.cstring = "GWProto"; //TODO gw->id ?
+	conn.cleansession = false;
+	conn.keepAliveInterval = 30;
+
 	for (;;)
 	{
-		osDelay(1000);
+		vTaskDelay(1000);
+		switch (MQTTClientState)
+		{
+		case MQTT_CLIENT_UNNINITIALIZED:
+		{
+			MQTTClientInit(&client, &network, MQTT_TIMEOUT_MS, txBuf,
+			MQTT_BUFFER_SIZE, rxBuf, MQTT_BUFFER_SIZE);
+			MQTTClientState = MQTT_CLIENT_DISCONNECTED;
+		}
+			break;
+		case MQTT_CLIENT_DISCONNECTED:
+		{
+			if (gw->Status == GW_BROKER_SOCKET_OPEN)
+			{
+				if (!MQTTConnect(&client, &conn))
+				{
+					MQTTClientState = MQTT_CLIENT_CONNECTED;
+
+					sprintf(topic, "/%lu/Info", (uint32_t) gw->Id);
+					MQTTSubscribe(&client, topic, QOS0, &MQTTHandler_Info);
+				}//TODO tratar erro de conexão MQTT
+			}
+		}
+			break;
+		case MQTT_CLIENT_CONNECTED:
+		{
+			TimerInit(&timer);
+
+#if defined(MQTT_TASK)
+			MutexLock(&gw->MQTTMutex);
+#endif
+			TimerCountdownMS(&timer, 500); /* Don't wait too long if no traffic is incoming */
+			MQTTYield(&client, 50);
+
+			while (uxQueueSpacesAvailable(gw->DataSourceQs.DataUsedQueue)
+					< OVS_NUMBER_DATA_BUFFER_SIZE)
+			{
+				uint8_t idx = 0;
+				uint8_t msgBuf[ChannelNumberData_size];
+
+				xQueueReceive(gw->DataSourceQs.DataUsedQueue, &idx, 0);
+
+				ChannelNumberData * sample = &gw->DataSampleBuffer[idx];
+
+				ostream = pb_ostream_from_buffer(msgBuf, sizeof(msgBuf));
+				pb_encode(&ostream, ChannelNumberData_fields, sample);
+				msg.qos = QOS0;
+				msg.payload = msgBuf;
+				msg.payloadlen = ostream.bytes_written;
+				sprintf(&topic, "/channel/%u/protobuf/data", sample->channelId);
+				MQTTPublish(&client, topic, &msg);//TODO Tratar erro de envio de mensagem MQTT
+
+				xQueueSend(gw->DataSourceQs.DataAvailableQueue, &idx, 0);
+			}
+
+#if defined(MQTT_TASK)
+			MutexUnlock(&gw->MQTTMutex);
+#endif
+		}
+			break;
+		default:
+			break;
+		}
+
 	}
 }
 
