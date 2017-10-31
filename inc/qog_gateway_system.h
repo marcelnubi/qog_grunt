0c12c3d96dba0b30d6e9b25d730e348e4e345e43
Marcel Mello <marcel.mello@qogni.com>
2017-10-31 13:34:07 -0200

    Pacote "System" com função de pegar UUI stm32f0

---
+++ inc/qog_gateway_system.h
@@ -0,0 +1,16 @@
+/*
+ * qog_gateway_system.h
+ *
+ *  Created on: Oct 31, 2017
+ *      Author: Marcel
+ */
+
+#ifndef INC_QOG_GATEWAY_SYSTEM_H_
+#define INC_QOG_GATEWAY_SYSTEM_H_
+
+#include "qog_ovs_gateway_internal_types.h"
+#include "qog_gateway_error_types.h"
+
+qog_gw_error_t qog_gw_sys_getUri(uint8_t *);
+
+#endif /* INC_QOG_GATEWAY_SYSTEM_H_ */
---
+++ src/qog_gateway_system.c
@@ -0,0 +1,25 @@
+/*
+ * qog_gateway_system.c
+ *
+ *  Created on: Oct 31, 2017
+ *      Author: Marcel
+ */
+
+#include "qog_gateway_system.h"
+
+#if defined (STM32F091xC)
+#include "stm32f0xx.h"
+#endif
+
+qog_gw_error_t qog_gw_sys_getUri(uint8_t * id)
+{
+	uint8_t bf[12];
+
+	memcpy(bf, (uint8_t *) UID_BASE, 12);
+	sprintf((char*) id, "%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+			bf[0], bf[1], bf[2], bf[3], bf[4], bf[5], bf[6], bf[7], bf[8],
+			bf[9], bf[10], bf[11]);
+	id[25]='\0';
+
+	return GW_e_OK;
+}
