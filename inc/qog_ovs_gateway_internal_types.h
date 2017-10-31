6d46dd04c15b82d71b498effb34c9f528067991e
Marcel Mello <marcel.mello@qogni.com>
2017-10-31 13:36:26 -0200

    gateway id como 24char + eos

--- inc/qog_ovs_gateway_internal_types.h
+++ inc/qog_ovs_gateway_internal_types.h
@@ -31,45 +31,41 @@ typedef SemaphoreHandle_t qog_Mutex;
 typedef TaskFunction_t qog_Task;
 typedef QueueHandle_t qog_Queue;
 //typedef uint32_t GatewayId;
-typedef struct GatewayId { uint32_t x[3]; } GatewayId;
+typedef struct GatewayId {
+	uint8_t x[24 + 1];
+} GatewayId;
 
-typedef struct
-{
+typedef struct {
 	uint32_t ChannelId;
 	uint32_t Timestamp;
 	double Value;
 } qog_DataSample;
 
-typedef struct
-{
+typedef struct {
 	qog_Queue DataAvailableQueue;
 	qog_Queue DataUsedQueue;
 } qog_DataSource_queues;
 
-typedef struct
-{
+typedef struct {
 	uint32_t Period;
 	uint8_t Parameters[32];
 } qog_DataSource_config;
 
 typedef struct Gateway Gateway;
 
-typedef struct
-{
+typedef struct {
 	qog_Task (*Task)(Gateway *gwInst);
 	uint32_t requestedHeap;
 	TaskHandle_t Handle;
 } qog_gateway_task;
 
-typedef struct
-{
+typedef struct {
 	uint8_t WLAN_SSID[32];
 	uint8_t WLAN_PSK[63];
 	uint8_t WLAN_AUTH;
 } WLANConnectionParams;
 
-typedef struct
-{
+typedef struct {
 	uint8_t HostName[32];
 	uint16_t HostPort;
 	uint32_t HostIp;
@@ -79,8 +75,7 @@ typedef struct
 
 typedef OVS_Channel DataChannel;
 
-typedef enum
-{
+typedef enum {
 	GW_ERROR = -1,
 	GW_STARTING,
 	GW_AP_CONFIG_MODE,
@@ -95,18 +90,14 @@ typedef enum
 
 typedef struct Gateway Gateway;
 
-typedef struct
-{
+typedef struct {
 	qog_gateway_task * WifiTask;
-	qog_gateway_task * MQTTClientTask;
 	qog_gateway_task * MQTTPublisherTask;
 	qog_gateway_task * DataSourceTask;
 	qog_gateway_task * LocalStorageTask;
-	qog_gateway_task * SystemTimeTask;
 } GatewayTasks;
 
-struct Gateway
-{
+struct Gateway {
 	GatewayId Id;
 	WLANConnectionParams WLANConnection;
 	BrokerParams BrokerParams;
