d8c4c0c46374421ab58248e49b2b413107ecd78b
Marcel Mello <marcel.mello@qogni.com>
2017-10-31 13:35:49 -0200

    format

--- src/qog_gateway_system.c
+++ src/qog_gateway_system.c
@@ -11,15 +11,14 @@
 #include "stm32f0xx.h"
 #endif
 
-qog_gw_error_t qog_gw_sys_getUri(uint8_t * id)
-{
+qog_gw_error_t qog_gw_sys_getUri(uint8_t * id) {
 	uint8_t bf[12];
 
 	memcpy(bf, (uint8_t *) UID_BASE, 12);
 	sprintf((char*) id, "%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
 			bf[0], bf[1], bf[2], bf[3], bf[4], bf[5], bf[6], bf[7], bf[8],
 			bf[9], bf[10], bf[11]);
-	id[25]='\0';
+	id[25] = '\0';
 
 	return GW_e_OK;
 }
