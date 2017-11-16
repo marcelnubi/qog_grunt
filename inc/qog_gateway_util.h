e24649d2d9687716d3562a406fa0291ee17d434b
Marcel Mello <marcel.mello@qogni.com>
2017-11-16 08:59:33 -0200

    Adicionando util de comparar EdgeId

---
+++ inc/qog_gateway_util.h
@@ -0,0 +1,15 @@
+/*
+ * qog_gateway_util.h
+ *
+ *  Created on: 16 Nov 2017
+ *      Author: marcel
+ */
+
+#ifndef INC_QOG_GATEWAY_UTIL_H_
+#define INC_QOG_GATEWAY_UTIL_H_
+
+#include "qog_ovs_gateway_internal_types.h"
+
+bool qog_gw_util_isEdgeEqual(Edge * e1, Edge *e2);
+
+#endif /* INC_QOG_GATEWAY_UTIL_H_ */
---
+++ src/qog_gateway_util.c
@@ -0,0 +1,54 @@
+/*
+ * qog_gateway_util.c
+ *
+ *  Created on: 16 Nov 2017
+ *      Author: marcel
+ */
+
+#include "qog_gateway_util.h"
+
+bool qog_gw_util_isEdgeEqual(Edge * ec1, Edge *ec2) {
+	bool ret = false;
+
+	if (ec1->Type != ec2->Type)
+		return ret;
+
+	pb_istream_t istream1 = pb_istream_from_buffer(ec1->Id.bytes,
+			sizeof(ec1->Id.bytes));
+	pb_istream_t istream2 = pb_istream_from_buffer(ec2->Id.bytes,
+			sizeof(ec2->Id.bytes));
+
+	switch (ec1->Type) {
+	case OVS_EdgeType_NULL_EDGE: {
+		ret = true;
+	}
+		break;
+	case OVS_EdgeType_QOGNI_IOB_EDGE: {
+		OVS_Edge_IOB_id e1, e2;
+		pb_decode(&istream1, OVS_Edge_IOB_id_fields, &e1);
+		pb_decode(&istream2, OVS_Edge_IOB_id_fields, &e2);
+
+		if (e1.Iob != e2.Iob) {
+			ret = false;
+			break;
+		}
+		if (e1.IobEdge != e2.IobEdge) {
+			ret = false;
+			break;
+		}
+		if (e1.URI.size != e2.URI.size) {
+			ret = false;
+			break;
+		}
+		if (e1.URI.bytes != e2.URI.bytes) {
+			ret = false;
+			break;
+		}
+	}
+		break;
+	default:
+		break;
+	}
+
+	return ret;
+}
