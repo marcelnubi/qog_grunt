3e7740633a3a10451050ac6892affd8dbd4b388e
Marcel Mello <marcel.mello@qogni.com>
2017-11-27 12:41:45 -0200

    removendo delay

--- Tasks/src/OVS_Gateway_Wifi_Task_ATWINC1500.c
+++ Tasks/src/OVS_Gateway_Wifi_Task_ATWINC1500.c
@@ -412,7 +412,7 @@ int SocketSend(uint8_t socketN, unsigned char * buf, int len, int timeout) {
 	vTaskSetTimeOutState(&xTimeOut);
 
 	//while (xTaskCheckForTimeOut(&xTimeOut, &xTicksToWait) == pdFALSE) {
-	HAL_Delay(1);
+	//HAL_Delay(1);
 	m2m_wifi_handle_events(NULL);
 	//	if (skTx > 0)
 	//break;
